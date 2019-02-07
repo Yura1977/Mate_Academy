c4c8042bf509e35b621c5cb7b0f8896989273585
Yura <verbitskey@gmail.com>
2019-02-07 02:36:12 +0200

    Add files via upload

---
+++ ClassBuilder/serialization/Circle.java
@@ -0,0 +1,22 @@
+package serialization;
+
+public class Circle implements Shape {
+    private int radius;
+
+    public Circle(int radius) {
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Circle radius:" + getRadius());
+    }
+}
