f9028c5bc8fb9db5ba5779da6e090da27cc621d2
Yura <verbitskey@gmail.com>
2019-02-07 02:33:45 +0200

    Add files via upload

---
+++ ClassBuilder/immutable/ComputerBuilderDirector.java
@@ -0,0 +1,12 @@
+package immutable;
+
+public class ComputerBuilderDirector {
+
+    public Computer getBasicComputer() {
+        return new Computer.Builder("2GB", "2TB", "Intel i5").build();
+    }
+
+    public Computer getGraphicsCardEnabledComputer() {
+    return new Computer.Builder("3GB", "3TB", "Intel i7").setGraphicsCardEnabled(true).build();
+    }
+}
