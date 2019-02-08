93ff856cea119729cf4ae0171f864ce4854332de
Yura <verbitskey@gmail.com>
2019-02-08 13:54:48 +0200

    Update Main.java

--- ClassBuilder/patternbuilder/Main.java
+++ ClassBuilder/patternbuilder/Main.java
@@ -3,7 +3,7 @@ package patternbuilder;
 public class Main {
     public static void main(String[] args) {
         Laptop laptop = new Laptop()
-                .setProcessor(Processor.AMD_Const)
+                .setProcessor(Processor.AMD)
                 .setHdd(102400)
                 .setRam(4 * 10240)
                 .setTouchDisplay(false);
