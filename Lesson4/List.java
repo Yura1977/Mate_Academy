fe25f2a3bf5ae34f8da9b03ee79fdaea9429b046
Yura <verbitskey@gmail.com>
2019-02-17 21:44:50 +0200

    Delete List.java

--- Lesson4/List.java
+++
@@ -1,9 +0,0 @@
-package collections;
-
-public interface List<T> extends Iterable<T> {
-    boolean add(T t);
-    void remove(int index);
-    T get(int index);
-    int size();
-    void update(int index, T t);
-}
