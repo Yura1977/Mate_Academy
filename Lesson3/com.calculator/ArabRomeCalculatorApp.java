4f482bca02b3661ba05029efdaa5db37a9ab116a
Yura <verbitskey@gmail.com>
2019-02-17 23:36:34 +0200

    Add files via upload

---
+++ Lesson3/com.calculator/ArabRomeCalculatorApp.java
@@ -0,0 +1,9 @@
+package com.calculator;
+
+public class
+ArabRomeCalculatorApp {
+    public static void main(String[] args) {
+        ArabRomeCalculator calculator = new ArabRomeCalculator();
+        calculator.run();
+    }
+}
