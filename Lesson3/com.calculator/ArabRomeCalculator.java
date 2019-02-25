62a73da3206b984f2106761adc3231dea0130be5
Yura <verbitskey@gmail.com>
2019-02-25 16:38:31 +0200

    Update ArabRomeCalculator.java

--- Lesson3/com.calculator/ArabRomeCalculator.java
+++ Lesson3/com.calculator/ArabRomeCalculator.java
@@ -2,18 +2,13 @@ package com.calculator;
 
 import java.util.Scanner;
 
-public class ArabRomeCalculator {
-    public void run() {
-        int[] numbers = new int[2];
+class ArabRomeCalculator {
+    void run() {
+        int[] numbers = new int[10];
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("Enter first number: ");
         String value1 = scanner.nextLine();
         numbers[0] = (!isDecimal(value1) ? getDecimalNumber(value1) : Integer.parseInt(value1));
-
-        System.out.print("Enter operation: ");
-        String operation = scanner.nextLine();
-
         System.out.print("Enter second number: ");
         String value2 = scanner.nextLine();
         numbers[1] = (isDecimal(value2) ? Integer.parseInt(value2) : getDecimalNumber(value2));
@@ -22,26 +17,15 @@ public class ArabRomeCalculator {
             System.out.println("Number have to be the same type!");
             return;
         }
-        int result = calculate(numbers[0], operation, numbers[1]);
-        System.out.print(value1 + " " + operation + " " + value2 + " = ");
-        System.out.println(isDecimal(value1) ? result: getRomeNumber(result));
-    }
-
-    private int calculate(int value1, String operation, int value2) {
-        switch (operation) {
-            case "+":
-                return value1 + value2;
-            case "-":
-                return value1 - value2;
-            case "*":
-                return value1 * value2;
-            case "/":
-                return value1 / value2;
-            default:
-                System.out.println("Illegal input");
-                return 0;
-
-        }
+        Operation expression = new Operation() {
+            @Override
+            public int calculate(int value11, int value21) {
+                return ((value11 * value21) + value11 - value21) / value11;
+            }
+        };
+        int result = expression.calculate(numbers[0], numbers[1]);
+        System.out.print(value1 + ", " + value2 + " = ");
+        System.out.println(isDecimal(value1) ? result : getRomeNumber(result));
     }
 
     private int getDecimalNumber(String number) {
@@ -58,18 +42,18 @@ public class ArabRomeCalculator {
     }
 
     private StringBuilder getRomeNumber(int n) {
-        int number =  n;
-        if  (number <= 0) {
+        int number = n;
+        if (number <= 0) {
             System.out.println("Number couldn't be negative!");
             return new StringBuilder();
         }
         StringBuilder result = new StringBuilder();
         if (number >= 1000) {
-            result.append(getRomeNumberPart("M", number/1000));
+            result.append(getRomeNumberPart("M", number / 1000));
             number %= 1000;
         }
         if (number >= 500 && number < 900) {
-            result.append(getRomeNumberPart("D", number/500));
+            result.append(getRomeNumberPart("D", number / 500));
             number %= 500;
         }
         if (number >= 500) {
@@ -77,7 +61,7 @@ public class ArabRomeCalculator {
             number %= 100;
         }
         if (number >= 100 && number < 400) {
-            result.append(getRomeNumberPart("C", number/100));
+            result.append(getRomeNumberPart("C", number / 100));
             number %= 100;
         }
         if (number >= 100) {
@@ -85,7 +69,7 @@ public class ArabRomeCalculator {
             number %= 100;
         }
         if (number >= 50 && number < 90) {
-            result.append(getRomeNumberPart("L", number/50));
+            result.append(getRomeNumberPart("L", number / 50));
             number %= 50;
         }
         if (number >= 50) {
@@ -93,7 +77,7 @@ public class ArabRomeCalculator {
             number %= 50;
         }
         if (number >= 10 && number < 40) {
-            result.append(getRomeNumberPart("X", number/10));
+            result.append(getRomeNumberPart("X", number / 10));
             number %= 10;
         }
         if (number >= 10) {
@@ -135,13 +119,32 @@ public class ArabRomeCalculator {
     }
 
     private int getValue(char symbol) {
-        if (symbol == 'I') { return 1; }
-        if (symbol == 'V') { return 5; }
-        if (symbol == 'X') { return 10; }
-        if (symbol == 'L') { return 50; }
-        if (symbol == 'C') { return 100; }
-        if (symbol == 'D') { return 500; }
-        if (symbol == 'M') { return 1000; }
+        if (symbol == 'I') {
+            return 1;
+        }
+        if (symbol == 'V') {
+            return 5;
+        }
+        if (symbol == 'X') {
+            return 10;
+        }
+        if (symbol == 'L') {
+            return 50;
+        }
+        if (symbol == 'C') {
+            return 100;
+        }
+        if (symbol == 'D') {
+            return 500;
+        }
+        if (symbol == 'M') {
+            return 1000;
+        }
         return -1;
     }
+}
+
+@FunctionalInterface
+interface Operation {
+    int calculate(int value1, int value2);
 }
