ed382268798d85abd63366b078ea2011dba69ec7
Yura <verbitskey@gmail.com>
2019-03-01 16:56:34 +0200

    Delete ArabRomeCalculatorApp.java

--- Lesson3/com.calculator/ArabRomeCalculatorApp.java
+++
@@ -1,9 +0,0 @@
-package com.calculator;
-
-public class
-ArabRomeCalculatorApp {
-    public static void main(String[] args) {
-        ArabRomeCalculator calculator = new ArabRomeCalculator();
-        calculator.run();
-    }
-}
